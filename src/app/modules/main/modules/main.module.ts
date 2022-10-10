2aae9ab1697aa8c6265998c91581182d9a0c109b
rocksJS <6abyghost@gmail.com>
2022-10-10 10:29:29 +0300

    trying to download branch

--- src/app/modules/main/modules/main.module.ts
+++ src/app/modules/main/modules/main.module.ts
@@ -4,9 +4,6 @@ import { MenuComponent } from '../components/menu/menu.component';
 import { SubheaderComponent } from '../components/subheader/subheader.component';
 import { MainComponent } from '../pages/main.component';
 
-import { BaseTableComponent } from 'src/app/shared/components/base-table/base-table.component';
-import { ProjectsFormComponent } from 'src/app/shared/components/projects-form/projects-form.component';
-
 import { MainRoutingModule } from './main.routing.module';
 
 @NgModule({
