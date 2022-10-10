62227a44a848fbdc2782e7261d857d8ae26d1b27
rocksJS <6abyghost@gmail.com>
2022-10-10 10:37:51 +0300

    with errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+    
 import { AppComponent } from './app.component';
 
 import { CommonModule, registerLocaleData } from '@angular/common';
