7538a2937039a5f7399d7961543672fb5495c27b
rmnsnpk <snapko0063@gmail.com>
2022-10-10 15:54:34 +0300

    fixed

--- src/app/shared/classes/base-input/base-input.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BaseInputComponent } from './base-input.component';
-
-@NgModule({
-  declarations: [BaseInputComponent],
-  imports: [CommonModule],
-})
-export class BaseInputModule {}
